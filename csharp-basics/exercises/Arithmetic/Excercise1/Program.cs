7f3f894a7372fdb9a152cce020d52d0556abe79b
ervinsapinis <larmantine@gmail.com>
2021-11-25 17:16:57 +0200

    uzdevums 1 done.

---
+++ csharp-basics/exercises/Arithmetic/Excercise1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Excercise1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your first integer:");
+            var firstInt = Console.ReadLine();
+            Console.WriteLine("Enter your second integer:");
+            var secondInt = Console.ReadLine();
+        }
+    }
+}
