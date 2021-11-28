63b6486c071c21967c5edecb3de06b4e4ff28e80
ervinsapinis <larmantine@gmail.com>
2021-11-28 20:04:25 +0200

    Exercise 10 done

--- csharp-basics/exercises/TypesAndVariables/SadFor/Program.cs
+++ csharp-basics/exercises/TypesAndVariables/SadFor/Program.cs
@@ -12,7 +12,7 @@ namespace SadFor
         private static void Test1()
         {
             //fixme so I would compile
-            /*
+            int i;
             for (i = 0; i < 10; i++)
             {
                 if (Math.Sqrt(i) > 2.5)
@@ -20,7 +20,7 @@ namespace SadFor
                     Console.WriteLine(i);
                 }
             }
-            */
+            
         }
     }
 }
