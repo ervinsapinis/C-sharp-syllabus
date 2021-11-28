cee37451cdd88af2bd4d4e903418f53aaaf9fbcb
ervinsapinis <larmantine@gmail.com>
2021-11-28 17:56:58 +0200

    Exercise4 done

---
+++ csharp-basics/exercises/TypesAndVariables/Exercise4/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Exercise4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your year of birth:");
+            short birthYear = short.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter your full name:");
+            string userName = Console.ReadLine();
+
+            Console.WriteLine("My name is " + userName + " and I was born in " + birthYear);
+        }
+    }
+}
