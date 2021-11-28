afcca0ce2668aa5ccb7a6bb4ee04e9e25c2de4a9
ervinsapinis <larmantine@gmail.com>
2021-11-28 19:39:28 +0200

    Exercise8 done

---
+++ csharp-basics/exercises/TypesAndVariables/Exercise8/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Exercise8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please input the desired amount of minutes:");
+            int minutes = int.Parse(Console.ReadLine());
+            int years = minutes / 525600;
+            int minutesLeftOver = minutes % 525600;
+            int days = minutesLeftOver / 1440;
+            Console.WriteLine(minutes + " are " + years + " years and about " + days + " days");
+        }
+    }
+}
