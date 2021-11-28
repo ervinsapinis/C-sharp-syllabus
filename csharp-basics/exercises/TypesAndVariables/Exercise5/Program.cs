1808613e59f2fe3067a6f76605afbff02035bfbc
ervinsapinis <larmantine@gmail.com>
2021-11-28 18:28:21 +0200

    Exercise5 done.

---
+++ csharp-basics/exercises/TypesAndVariables/Exercise5/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Exercise5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("+------------------------------------------------------------+");
+            Console.WriteLine("{0,5} {1,26} {2,28}", "|1|", "Latvian |", "Mr. Lapinskis |");
+            Console.WriteLine("{0,5} {1,26} {2,28}", "|2|", "Russian |", "Ms. Gorbacheva |");
+            Console.WriteLine("{0,5} {1,26} {2,28}", "|3|", "German |", "Dr. Prof. Hermann |");
+            Console.WriteLine("{0,5} {1,26} {2,28}", "|4|", "Rocket Sciene |", "Mr. Armstrong |");
+            Console.WriteLine("{0,5} {1,26} {2,28}", "|5|", "Biotechnology |", "Ms. Merkel |");
+            Console.WriteLine("{0,5} {1,26} {2,28}", "|6|", "Botanics |", "Mrs. Wong |");
+            Console.WriteLine("{0,5} {1,26} {2,28}", "|7|", "Advanced Math |", "Mrs. Lebedeva |");
+            Console.WriteLine("+------------------------------------------------------------+");
+        }
+    }
+}
