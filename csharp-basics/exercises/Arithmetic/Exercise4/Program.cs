3824b38945cd423d8f8a1698053186f0bf1d3810
ervinsapinis <larmantine@gmail.com>
2021-11-29 11:25:47 +0200

    Exercise4 done

---
+++ csharp-basics/exercises/Arithmetic/Exercise4/Program.cs
@@ -0,0 +1,20 @@
+﻿    using System;
+
+namespace Exercise4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int product = 1;
+            int input = 10;
+
+            for (int i = 1; i <= input; i++)
+            {
+                product *= i;
+            }
+
+            Console.WriteLine(product);
+        }
+    }
+}
