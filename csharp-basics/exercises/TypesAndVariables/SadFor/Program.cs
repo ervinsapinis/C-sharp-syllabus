5f8d66cb5e08fca7f0d990cfadbf6a5e35b6f4b4
ervinsapinis <larmantine@gmail.com>
2021-11-29 17:44:35 +0200

    fixed exercise 10:
    removed comment and an empty line

--- csharp-basics/exercises/TypesAndVariables/SadFor/Program.cs
+++ csharp-basics/exercises/TypesAndVariables/SadFor/Program.cs
@@ -11,7 +11,6 @@ namespace SadFor
 
         private static void Test1()
         {
-            //fixme so I would compile
             int i;
             for (i = 0; i < 10; i++)
             {
@@ -20,7 +19,6 @@ namespace SadFor
                     Console.WriteLine(i);
                 }
             }
-            
         }
     }
 }
