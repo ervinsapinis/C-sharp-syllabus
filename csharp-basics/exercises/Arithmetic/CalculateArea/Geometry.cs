0f73229827217685e1ce7ce9a208707575e70a94
ervinsapinis <larmantine@gmail.com>
2021-11-30 11:11:54 +0200

    exercise 10 done

--- csharp-basics/exercises/Arithmetic/CalculateArea/Geometry.cs
+++ csharp-basics/exercises/Arithmetic/CalculateArea/Geometry.cs
@@ -8,19 +8,22 @@ namespace CalculateArea
 {
     public class Geometry
     {
-        public static double AreaOfCircle(decimal radius)
+        public static double AreaOfCircle(int radius)
         {
-            throw new NotImplementedException();
+            double area = Math.PI * radius * radius;
+            return Math.Round(area, 2);
         }
 
-        public static double AreaOfRectangle(decimal length, decimal width)
+        public static double AreaOfRectangle(double length, double width)
         {
-            throw new NotImplementedException();
+            double area = (double)length * width;
+            return area;
         }
 
-        public static double AreaOfTriangle(decimal ground, decimal h)
+        public static double AreaOfTriangle(double ground, double height)
         {
-            throw new NotImplementedException();
+            double area = (ground * height) / 2;
+            return area;
         }
     }
 }
