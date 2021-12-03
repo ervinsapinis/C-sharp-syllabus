e35dfdfe9a13e59fafd3eadc99e6c96fc6eb3e58
ervinsapinis <larmantine@gmail.com>
2021-12-04 01:08:41 +0200

    Exercise4 done

---
+++ csharp-basics/exercises/FlowOfControl/FlowControl/Exercise4/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Exercise4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please input a number for a day: ");
+            int userInput = int.Parse(Console.ReadLine());
+
+            if (userInput <= 6 && userInput >= 0)
+            {
+                if (userInput == 0)
+                {
+                    Console.WriteLine("Sunday");
+                }
+                if (userInput == 1)
+                {
+                    Console.WriteLine("Monday");
+                }
+                if (userInput == 2)
+                {
+                    Console.WriteLine("Tuesday");
+                }
+                if (userInput == 3)
+                {
+                    Console.WriteLine("Wednesday");
+                }
+                if (userInput == 4)
+                {
+                    Console.WriteLine("Thursday");
+                }
+                if (userInput == 5)
+                {
+                    Console.WriteLine("Saturday");
+                }
+                if (userInput == 6)
+                {
+                    Console.WriteLine("Sunday");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Not a valid day");
+            }
+        }
+    }
+}
