81cbc90e021c4c77bb5aad23107a8f61507b7f0a
ervinsapinis <larmantine@gmail.com>
2021-12-07 21:20:18 +0200

    Exercise5 done

--- csharp-basics/exercises/Arrays/Arrays/Exercise5/Program.cs
+++ csharp-basics/exercises/Arrays/Arrays/Exercise5/Program.cs
@@ -13,15 +13,9 @@ namespace Exercise5
         {
             int[] myArray = {25, 14, 56, 15, 36, 56, 77, 18, 29, 49};
 
-            var indexOfElement36 = -1;
-            var indexOfElement29 = -1;
+            var indexOfElement36 = Array.IndexOf(myArray, 36);
+            var indexOfElement29 = Array.IndexOf(myArray, 29);
 
-            /*
-            fixme
-            for (?) {
-                
-            }
-            */
             Console.WriteLine("Index position of 36 is: " + indexOfElement36);
             Console.WriteLine("Index position of 29 is: " + indexOfElement29);
             
