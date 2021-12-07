7ef5a1c168aa5074ca35c3e88a64d23d71241e59
ervinsapinis <larmantine@gmail.com>
2021-12-08 00:58:08 +0200

    Exercise8 done

---
+++ csharp-basics/exercises/Arrays/Arrays/Exercise8/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+namespace Exercise8
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            string[] wordList =
+            {
+                "Archipelago", "Astrophysics", "Reptilian", "Processor"
+            };
+
+            var rnd = new Random();
+
+            string word = wordList[rnd.Next(wordList.Length)];
+            char[] wordChars = word.ToCharArray();
+
+            string guessableWord = new string(c:'_', word.Length);
+            char[] guessableWordArr = guessableWord.ToCharArray();
+
+            char[] misses = new char[5];
+            var missCounter = 0;
+
+            while (Array.IndexOf(guessableWordArr, '_') != -1)
+            {
+                Console.WriteLine("Word: {0}", string.Join(" ", guessableWordArr));
+                Console.WriteLine("Misses: {0}", string.Join(" ", misses));
+                var input = Console.ReadKey();
+                var guess = input.KeyChar;
+                Console.WriteLine();
+                Console.Write($"Guess: {guess}");
+
+
+                if (WordContains(word, guess))
+                {
+                    for (int i = 0; i < word.Length; i++)
+                    {
+                        if (word.ToLower()[i] == guess)
+                        {
+                            guessableWordArr[i] = wordChars[i];
+                        }
+                    }
+                }
+                else
+                {
+                    misses[missCounter] = guess;
+                    missCounter++;
+                }
+
+                if (missCounter > 4)
+                {
+                    Console.Clear();
+                    Console.WriteLine("You lost!");
+                    break;
+                }
+                else if (Array.IndexOf(guessableWordArr, '_') == -1)
+                {
+                    Console.Clear();
+                    Console.WriteLine("YOU GOT IT!");
+                    Console.WriteLine($"The word was: {word}");
+                    break;
+                }
+            }
+
+            Console.WriteLine("Play again? Y/N");
+            char restartChar = Console.ReadKey().KeyChar;
+            Restart(restartChar);
+
+        }
+
+        private static void Restart(char input)
+        {
+            if (input == 'y')
+            {
+                Console.Clear();
+                Main();
+            }
+            else
+            {
+                Console.Clear();
+                Console.WriteLine("Bye!");
+            }
+        }
+
+        private static bool WordContains(string word, char guess)
+        {
+            return word.ToLower().IndexOf(guess) > -1;
+        }
+
+        private static void Game()
+        {}
+
+    }
+}
