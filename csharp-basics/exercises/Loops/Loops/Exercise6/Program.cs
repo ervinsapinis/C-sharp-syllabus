6d5b58c324f221f722be5cf5465a055746ea78df
ervinsapinis <larmantine@gmail.com>
2021-12-09 21:42:05 +0200

    Exercise6 done

---
+++ csharp-basics/exercises/Loops/Loops/Exercise6/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Threading.Channels;
+
+namespace Exercise6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input;
+            int inputInt;
+            bool isBool;
+
+            Console.WriteLine("Max value?");
+            input = Console.ReadLine();
+            isBool = int.TryParse(input, out inputInt);
+
+            while (!isBool)
+            {
+                Console.WriteLine("Please input an integer.");
+                input = Console.ReadLine();
+                isBool = int.TryParse(input, out inputInt);
+            }
+
+            for (int i = 1; i <= inputInt; i++)
+            {
+                if (i % 20 == 0 && i % 5 == 0)
+                {
+                    Buzz();
+                    Console.WriteLine();
+                }
+                else if (i % 5 == 0 && i % 3 == 0)
+                {
+                    FizzBuzz();
+                }
+                else if (i % 5 == 0)
+                {
+                    Buzz();
+                }
+                else if (i % 3 == 0)
+                {
+                    Fizz();
+                }
+                else
+                {
+                    Console.Write(i+" ");
+                }
+            }
+        }
+
+        static void Fizz()
+        {
+            Console.Write("Fizz ");
+        }
+        static void Buzz()
+        {
+            Console.Write("Buzz ");
+        }
+        static void FizzBuzz()
+        {
+            Console.Write("FizzBuzz ");
+        }
+    }
+}
