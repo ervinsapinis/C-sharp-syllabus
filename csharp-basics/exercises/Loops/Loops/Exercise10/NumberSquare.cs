88d39daa8c56877ed6b6183ed86e679df3ff6d0e
ervinsapinis <larmantine@gmail.com>
2021-12-11 20:20:49 +0200

    Exercise10 done.

---
+++ csharp-basics/exercises/Loops/Loops/Exercise10/NumberSquare.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise10
+{
+    internal class NumberSquare
+    {
+        public static void Call()
+        {
+            Console.WriteLine("Please input min value");
+            int min = int.Parse(Console.ReadLine());
+            Console.WriteLine("Please input max value");
+            int max = int.Parse(Console.ReadLine());
+            int a = min;
+
+
+            for (int row = 0; row < max; row++)
+            {
+                for (int col = 0; col < max; col++)
+                {
+                    Console.Write(a);
+                    a++;
+                    if (a == max+1)
+                    {
+                        a -=max;
+                    }
+                }
+                a++;
+                Console.WriteLine();
+            }
+        }
+    }
+}
