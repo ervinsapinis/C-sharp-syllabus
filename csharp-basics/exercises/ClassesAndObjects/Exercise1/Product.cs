752b0621fae5508ec80f1e62b516baab416eda10
ervinsapinis <larmantine@gmail.com>
2021-12-16 00:04:35 +0200

    Exercise1 done

--- csharp-basics/exercises/ClassesAndObjects/Exercise1/Product.cs
+++ csharp-basics/exercises/ClassesAndObjects/Exercise1/Product.cs
@@ -8,5 +8,33 @@ namespace Exercise1
 {
     internal class Product
     {
+        //fields
+        private string _name;
+        private double _priceAtStart;
+        private int _amountAtStart;
+
+        //constructor
+        public Product(string name, double priceAtStart, int amountAtStart)
+        {
+            this._name = name;
+            this._priceAtStart = priceAtStart;
+            this._amountAtStart = amountAtStart;
+        }
+
+        //methods
+        public void PrintProduct()
+        {
+            Console.WriteLine($"{_name}, price: {_priceAtStart} EUR, amount: {_amountAtStart} units");
+        }
+
+        public void ChangeQuantity(int amount)
+        {
+            this._amountAtStart = amount;
+        }
+
+        public void ChangePrice(double amount)
+        {
+            this._priceAtStart = amount;
+        }
     }
 }
--- csharp-basics/exercises/ClassesAndObjects/Exercise1/Program.cs
+++ csharp-basics/exercises/ClassesAndObjects/Exercise1/Program.cs
@@ -6,7 +6,17 @@ namespace Exercise1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Product mouseProduct = new Product("Logitech mouse", 70, 14);
+            Product iphoneProduct = new Product("iPhone 5s", 999.99, 3);
+            Product printerProduct = new Product("Epson EB-U05", 440.46, 1);
+
+            mouseProduct.PrintProduct();
+            iphoneProduct.PrintProduct();
+            printerProduct.PrintProduct();
+
+            mouseProduct.ChangeQuantity(16);
+            mouseProduct.ChangePrice(69.99);
+            mouseProduct.PrintProduct();
         }
     }
 }
