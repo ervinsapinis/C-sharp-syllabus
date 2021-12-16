177fdda58601feb798191290b8d90c3f49f2e365
ervinsapinis <larmantine@gmail.com>
2021-12-16 19:29:56 +0200

    Exercise9 done

---
+++ csharp-basics/exercises/ClassesAndObjects/Exercise9/Point.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise9
+{
+    internal class Point
+    {
+        private int _x;
+        private int _y;
+
+        public Point(int xValue, int yValue)
+        {
+            _x = xValue;
+            _y = yValue;
+        }
+
+        public int X
+        {
+            get { return _x; }
+            set { _x = value; }
+        }
+
+        public int Y
+        {
+            get { return _y; }
+            set { _y = value; }
+        }
+    }
+}
