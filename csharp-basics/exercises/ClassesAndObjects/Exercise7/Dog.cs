cb2c512a532ca7038aabbbdb9decd701c32a37ca
ervinsapinis <larmantine@gmail.com>
2021-12-16 16:11:44 +0200

    Exercise7 done

---
+++ csharp-basics/exercises/ClassesAndObjects/Exercise7/Dog.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise7
+{
+    internal class Dog
+    {
+        //fields
+        private string _name;
+        private string _sex;
+        private Dog _motherDog;
+        private Dog _fatherDog;
+
+        //ctor
+        public Dog(string name, string sex, Dog motherDog, Dog fatherDog)
+        {
+            _name = name;
+            _sex = sex;
+            _motherDog = motherDog;
+            _fatherDog = fatherDog;
+        }
+        public Dog(string name, string sex)
+        {
+            _name = name;
+            _sex = sex;
+            _motherDog = null;
+            _fatherDog = null;
+        }
+        //properties
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+
+        public string Sex
+        {
+            get { return _sex; }
+            set { _sex = value; }
+        }
+
+        public Dog MotherDog
+        {
+            get { return _motherDog;}
+            set { _motherDog = value; }
+        }
+
+        public Dog FatherDog
+        {
+            get { return _fatherDog; }
+            set { _fatherDog = value; }
+        }
+
+        //methods
+        public string FathersName()
+        {
+            if (FatherDog == null)
+                return "Unknown";
+            else
+                return $"{FatherDog.Name}";
+        }
+
+        public bool HasSameMotherAs(Dog otherDog)
+        {
+            if (otherDog.MotherDog == this.MotherDog)
+                return true;
+            else
+                return false;
+        }
+    }
+}
