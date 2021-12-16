74121a930ca1a6b3f0047c36063f9b1a75428c92
ervinsapinis <larmantine@gmail.com>
2021-12-16 21:05:16 +0200

    Exercise10 done

---
+++ csharp-basics/exercises/ClassesAndObjects/BankAccount/BankAccount.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankAccount
+{
+    internal class BankAccount
+    {
+        private string _name;
+        private double _balance;
+
+        public BankAccount(string name, double balance)     
+        {
+            _name = name;
+            _balance = balance;
+        }
+
+        public string Name
+        {
+            get { return _name; }
+        }
+
+        public double Balance
+        {
+            get { return _balance; }
+        }
+
+        public string ShowUserNameAndBalance()
+        {
+            string decimalBalance = Math.Abs(Balance).ToString("0.00");
+            if(Balance < 0)
+                return $"{Name}, -${decimalBalance}"; 
+            return $"{Name}, ${decimalBalance}";
+        }
+    }
+}
