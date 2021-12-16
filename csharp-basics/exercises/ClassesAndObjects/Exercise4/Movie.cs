1dbb31144950429061c4b467d45a9e769e88568a
ervinsapinis <larmantine@gmail.com>
2021-12-16 14:35:03 +0200

    Exercise4

---
+++ csharp-basics/exercises/ClassesAndObjects/Exercise4/Movie.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise4
+{
+    internal class Movie
+    {
+        //fields
+        private string _title;
+        private string _studio;
+        private string _rating;
+
+        //ctor
+        public Movie(string title, string studio, string rating)
+        {
+            _title = title;
+            _studio = studio;
+            _rating = rating;
+        }
+
+        public Movie(string title, string studio)
+        {
+            _title = title;
+            _studio = studio;
+            _rating = "PG";
+        }
+
+        //properties
+        public string Title
+        {
+            get { return _title; }
+            set { _title = value; }
+
+        }
+        public string Studio
+        {
+            get { return _studio; }
+            set { _studio = value; }
+        }
+        public string Rating
+        {
+            get { return _rating; }
+            set { _rating = value; }
+        }
+
+        //methods
+        public string DisplayInfo()
+        {
+            return $"{Title}, {Studio}, {Rating}";
+        }
+    }
+}
---
+++ csharp-basics/exercises/ClassesAndObjects/Exercise4/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+
+namespace Exercise4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Movie bondMovie = new Movie("Casino Royale", "Eon Productions", "PG13");
+            Movie glassMovie = new Movie("Glass", "Buena Vista International", "PG13");
+            Movie spiderMovie = new Movie("Spider-Man: Into the Spider-Verse", "Columbia Pictures", "PG");
+            Movie batMovie = new Movie("Batman", "Columbia Pictures", "PG");
+
+            Movie[] allmMovies = {bondMovie, glassMovie, spiderMovie, batMovie};
+            Movie[] filteredMovies = GetPG(allmMovies);
+            foreach (Movie movie in filteredMovies)
+            {
+                Console.WriteLine(movie.DisplayInfo());
+            }
+
+        }
+        public static Movie[] GetPG(Movie[] movies)
+        {
+            var pgMovies = new List<Movie>();
+            foreach (var movie in movies)
+            {
+                if (movie.Rating == "PG")
+                {
+                    pgMovies.Add(movie);
+                }    
+            }
+            return pgMovies.ToArray();
+        }
+    }
+}
