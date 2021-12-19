1753344a1e740ef64f1f417d0441968baf2c1f04
ervinsapinis <larmantine@gmail.com>
2021-12-19 14:03:28 +0200

    Exercise3 done

---
+++ csharp-basics/exercises/Collections/Exercise3/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+namespace Exercise3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var namesList = new List<string>();
+            while (true)
+            {
+                Console.Write("Enter name: ");
+                string name = Console.ReadLine();
+                if (name != "")
+                    namesList.Add(name);
+                else
+                    break;
+            }
+
+            var uniqueNamesList = namesList.Distinct();
+            Console.WriteLine("Unique name list contains: " + string.Join(" ", uniqueNamesList));
+            Console.ReadKey();
+        }
+    }
+}
