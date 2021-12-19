83fc31678946899474c0e78a0860951b555833cf
ervinsapinis <larmantine@gmail.com>
2021-12-20 01:42:10 +0200

    Exercise8 done?

--- csharp-basics/exercises/Collections/Phonebook/PhoneEntry.cs
+++ csharp-basics/exercises/Collections/Phonebook/PhoneEntry.cs
@@ -1,8 +1,17 @@
+using System;
+using System.Collections.Specialized;
+
 namespace PhoneBook
 {
     public class PhoneEntry
     {
         public string name;
         public string number;
+
+        public PhoneEntry(string name, string number)
+        { 
+            this.number = number;
+            this.name = name;
+        }
     }
 }
--- csharp-basics/exercises/Collections/Phonebook/Program.cs
+++ csharp-basics/exercises/Collections/Phonebook/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using PhoneBook;
 
 namespace Phonebook
 {
@@ -10,6 +11,15 @@ namespace Phonebook
     {
         static void Main(string[] args)
         {
+            PhoneDirectory mydiDirectory = new PhoneDirectory();
+
+
+            mydiDirectory.AddEntry("Janis Choplin", "654-234-4956");
+            mydiDirectory.AddEntry("Humphrey Humphrey", "345-754-3456");
+            mydiDirectory.AddEntry("Ahmed Attalay", "764-867-4956");
+            mydiDirectory.AddEntry("Phil Biggs", "906-346-4956");
+            mydiDirectory.AddEntry("Ann Damsel", "667-234-4956");
+
         }
     }
 }
