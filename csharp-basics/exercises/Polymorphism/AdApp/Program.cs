34367d3f04b92f38c11a7b3e1cb56d15c91cc93a
ervinsapinis <larmantine@gmail.com>
2021-12-25 04:09:26 +0200

    Exercise5 AdApp done

--- csharp-basics/exercises/Polymorphism/AdApp/Program.cs
+++ csharp-basics/exercises/Polymorphism/AdApp/Program.cs
@@ -10,7 +10,8 @@ namespace AdApp
             c.AddAdvert(new Advert(1000));
             c.AddAdvert(new Hoarding(500, 7, 200));
             c.AddAdvert(new NewspaperAd(0, 30, 20));
-            c.AddAdvert(new TVAd(50000, 1000, 30, true));
+            c.AddAdvert(new TVAd( 1000, 30, true));
+            c.AddAdvert(new Poster(100, 20, 5));
             Console.WriteLine(c);
         }
     }
