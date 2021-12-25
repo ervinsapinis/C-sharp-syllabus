67f7c91b86746728c7a87034b8cbd84a9f4a8802
ervinsapinis <larmantine@gmail.com>
2021-12-25 03:26:08 +0200

    Exercise4 Firm done

---
+++ csharp-basics/exercises/Polymorphism/Firm/Commission.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Firm
+{
+     class Commission : Hourly
+     {
+         protected double _totalSales;
+         protected double _commissionRate;
+        public Commission(string eName, string eAddress, string ePhone, string socSecNumber, double rate, double commissionRate)
+            : base(eName, eAddress, ePhone, socSecNumber, rate)
+        {
+            _commissionRate = commissionRate;
+        }
+
+        public void AddSales(double totalSales)
+        {
+            _totalSales += totalSales;
+        }
+
+        public override double Pay()
+        {
+            var salary = base.Pay() + (_totalSales + _commissionRate*0.01);
+            _totalSales = 0;
+            return Math.Round(salary, 2);
+        }
+     }
+}
--- csharp-basics/exercises/Polymorphism/Firm/Staff.cs
+++ csharp-basics/exercises/Polymorphism/Firm/Staff.cs
@@ -11,7 +11,7 @@ namespace Firm
         //-----------------------------------------------------------------
         public Staff()
         {
-            staffList = new StaffMember[6];
+            staffList = new StaffMember[8];
             staffList[0] = new Executive("Sam", "123 Main Line",
                 "555-0469", "123-45-6789", 2423.07);
             staffList[1] = new Employee("Carla", "456 Off Line",
@@ -24,8 +24,16 @@ namespace Firm
                 "555-8374");
             staffList[5] = new Volunteer("Cliff", "321 Duds Lane",
                 "555-7282");
+            staffList[6] = new Commission("Pontius", "Jerusalem, Imperial Palace",
+                "555-7282", "890-20-3540", 6.25, .2);
+            staffList[7] = new Commission("Biggus", "Rome, Imperial Palace",
+                "555-7282", "120-50-1234", 9.75, .15);
             ((Executive) staffList[0]).AwardBonus(500.00);
-            ((Hourly) staffList[3]).AddHours(40);
+            ((Hourly)staffList[3]).AddHours(40);
+            ((Commission)staffList[6]).AddHours(35);
+            ((Commission)staffList[7]).AddHours(40);
+            ((Commission)staffList[6]).AddSales(400);
+            ((Commission)staffList[7]).AddSales(950);
         }
 
         //-----------------------------------------------------------------
