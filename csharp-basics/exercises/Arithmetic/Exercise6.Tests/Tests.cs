467fc7438657540aa5372d8e845eef0f2da199e5
ervinsapinis <larmantine@gmail.com>
2022-01-05 01:08:25 +0200

    Exercise6.Tests Started

---
+++ csharp-basics/exercises/Arithmetic/Exercise6.Tests/Tests.cs
@@ -0,0 +1,17 @@
+using System;
+using Xunit;
+
+namespace Exercise6.Tests
+{
+    public class Tests
+    {
+        [Fact]
+        public void Printer_3_ShouldReturnCoza()
+        {
+            // Arrange
+            // Act
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+    }
+}
