f670786611ee0ff0622c372ad7e018ce9e63d672
ervinsapinis <larmantine@gmail.com>
2022-01-05 16:31:17 +0200

    GravityCalculator.Tests done

--- csharp-basics/exercises/Arithmetic/GravityCalculator/Program.cs
+++ csharp-basics/exercises/Arithmetic/GravityCalculator/Program.cs
@@ -7,13 +7,37 @@ namespace GravityCalculator
     {
         static void Main(string[] args)
         {
-            double gravity = -9.81;  // Earth's gravity in m/s^2
             double initialVelocity = 0.0;
             double fallingTime = 10.0;
             double initialPosition = 0.0;
-            double finalPosition = 0.5 * (gravity * Math.Pow(fallingTime, 2)) + (initialVelocity * fallingTime) + initialPosition;
-            Console.WriteLine("The object's position after " + fallingTime + " seconds is " + finalPosition + " m.");
+            var earth = new EarthGravityCalculator(initialVelocity, fallingTime, initialPosition);
+            Console.WriteLine(earth.ReportResult());
             Console.ReadKey();
+        }
+    }
+
+    public class EarthGravityCalculator
+    {
+        private const double _gravity = -9.81;  // Earth's gravity in m/s^2
+        private double _initialVelocity;
+        private double _fallingTime;
+        private double _initialPosition;
+
+        public EarthGravityCalculator(double initialVelocity, double fallingTime, double initialPosition)
+        {
+            _initialVelocity = initialVelocity;
+            _fallingTime = fallingTime;
+            _initialPosition = initialPosition;
+        }
+
+        private double Calculate()
+        {
+            return 0.5 * (_gravity * Math.Pow(_fallingTime, 2)) + (_initialVelocity * _fallingTime) + _initialPosition;
+        }
+
+        public string ReportResult()
+        {
+            return "The object's position after " + _fallingTime + " seconds is " + Calculate() + " m.";
         }
     }
 }
---
+++ csharp-basics/exercises/Arithmetic/GravityCalculatorTests/EarthGravityCalculatorTests.cs
@@ -0,0 +1,25 @@
+﻿using Xunit;
+using GravityCalculator;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GravityCalculator.Tests
+{
+    public class EarthGravityCalculatorTests
+    {
+        [Fact()]
+        public void Calculate_StandstillObjectFalling10sec_()
+        {
+            Assert.True(false, "This test needs an implementation");
+        }
+
+        [Fact()]
+        public void ReportResultTest()
+        {
+            Assert.True(false, "This test needs an implementation");
+        }
+    }
+}
